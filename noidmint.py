770a418529875d60718a1d106d422fb170ae2d5f
Stephen Appel <srappel930@gmail.com>
2023-06-12 15:50:03 -0500

    create noidmint.py

---
+++ noidmint.py
@@ -0,0 +1,32 @@
+# noidmint.py
+# Stephen Appel 6/12/2023
+# Mint's a ark:id using our noid minting service at UWM Libraries
+
+import requests
+import re
+
+def searchForID(text) -> str:
+    # Will match the whole ID
+    # Group 1 will be the *Name Assigning Authority Number*
+    # Group 2 will be the *Assigned Name*
+    arkregex = re.compile(r"(\d{5})\/(\w{11})")
+    arkid = arkregex.search(text)
+    return arkid[0]
+
+def mintArk(minter) -> str:
+    try:
+        r = requests.get(minter)
+    except:
+        print("There was a connection error! Check the URL you used to request the id!")
+        return
+
+    if r.status_code != 200:
+        print("There was a non-200 status code from the minter: " + r.status_code)
+        return
+    else:
+        minter_text = r.text
+        arkid = searchForID(minter_text)
+        return arkid
+
+if __name__ == "__main__":
+    print(mintArk('https://digilib-dev.uwm.edu/noidu_gmgs?mint+1'))
